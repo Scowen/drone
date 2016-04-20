f6fcd8fa06bc98484439db2d4b754b23a5d7a8dd
Scowen <lmscowen@gmail.com>
2016-04-20 20:28:59 +0100

    Camera now follows & is locked with Left & Right rotation but free on the Up & Down

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,32 +2,22 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-	// [Range(400,600)]
 	public float minThrust = 460;
-	// [Range(400,600)]
 	public float idleThrust = 490;
-	// [Range(400,600)]
 	public float maxThrust = 510;
-// 	[Range(400,600)]
-	public float bankSpeed = 40;
-
+	public float acceleration = 500;
+	public float bankSpeed = 60;
 	public float rotateSpeed = 1.2f;
-
-
-	// The maximum that the drone can tilt in degrees.
 	public float maxTilt = 45;
 
 	private Rigidbody rb;
 	private float thrust;
 
-	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();	
 		thrust = idleThrust;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.W)) {
 			thrust = maxThrust;
