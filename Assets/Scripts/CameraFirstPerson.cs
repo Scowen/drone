1033399b1e10fd1799e272f5c97b058bbe3d693b
Xylum <lmscowen@gmail.com>
2016-04-21 15:27:43 +0100

    Stuff from this morning.

---
+++ Assets/Scripts/CameraFirstPerson.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFirstPerson : MonoBehaviour {
+
+	public GameObject player;
+
+	private Camera cameraComponent;
+
+	// Use this for initialization
+	void Start () {
+		transform.parent = player.transform;
+		transform.position = new Vector3 (0, 0, 0);
+		cameraComponent = GetComponent<Camera> ();
+		cameraComponent.fieldOfView = 80;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,58 +2,92 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public float minThrust = 460;
-	public float idleThrust = 490;
-	public float maxThrust = 510;
-	public float acceleration = 500;
-	public float bankSpeed = 60;
-	public float rotateSpeed = 1.2f;
-	public float maxTilt = 45;
+	public float minThrust = 50; // The minimum thrust that can be applied.
+	public float idleThrust = 490.5f; // The amount needed to make the drone hover.
+	public float maxThrust = 800; // The maximum thrust that can be applied.
+	public float rotateSpeed = 4.2f; // The speed in which the drone can rotate.
+	public float tiltSpeed = 4.5f; // The speed in which the drone will try to bank and pitch.
+	public float maxTilt = 65.0f; // The maximum angle in degrees that the drone may tilt to.
 
 	private Rigidbody rb;
-	private float thrust;
+	private float thrust; // The level of thrust being applied to the drone.
+	private bool hover = true; // The Drone will hover using the idleThrust variable.
+	private bool banking = false; // If the drone is banking at all.
+	private bool flipLock = true; // If the drone should be allowed to flip, toggleable.
+
+	private float angleBank; // The desired angle of the bank.
+	private float anglePitch; // The desired angle of the pitch.
+	private float angleYaw; // The desired angle of the yaw.
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();	
-		thrust = idleThrust;
+
+		// Need to set the angleYaw, otherwise it will "null out".
+		angleYaw = 0;
 	}
 	
 	void Update () {
+		// Hover toggle.
+		if (Input.GetKey (KeyCode.H)) 
+			hover = hover == true ? false : true;
+
+		// Flip toggle.
+		if (Input.GetKey (KeyCode.F)) 
+			flipLock = flipLock == true ? false : true;
+
+		// Thrust control.
 		if (Input.GetKey (KeyCode.W)) {
+			// Apply the max thrust.
 			thrust = maxThrust;
-		} else if (Input.GetKey(KeyCode.S)) {
+			hover = false;
+		} else if (Input.GetKey(KeyCode.S) || hover == false) {
+			// Apply minimum thrust.
 			thrust = minThrust;
+			hover = false;
 		} else {
+			// Hover.
 			thrust = idleThrust;
 		}
+
+		// Angles for banking upon key press.
+		if (Input.GetKey (KeyCode.LeftArrow))
+			angleBank = 360 - maxTilt;
+		else if (Input.GetKey (KeyCode.RightArrow))
+			angleBank = maxTilt;
+		else
+			angleBank = 0;
+
+		// Angles for pitching upon key press.
+		if (Input.GetKey (KeyCode.DownArrow))
+			anglePitch = 360 - maxTilt;
+		else if (Input.GetKey (KeyCode.UpArrow))
+			anglePitch = maxTilt;
+		else
+			anglePitch = 0;
+
+		if (Input.GetKey (KeyCode.A))
+			angleYaw -= rotateSpeed;
+		else if (Input.GetKey (KeyCode.D))
+			angleYaw += rotateSpeed;
 	}
 
-	// Any force updates.
+	// Any physics updates.
 	void FixedUpdate() {
+		// Apply thrust.
 		rb.AddForce (transform.up * thrust * Time.deltaTime);
 
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Rotate (Vector3.left * bankSpeed * Time.deltaTime);
-		}
+		// Tell the drone what angle we would like it to bank to.
+		Vector3 targetAngle = new Vector3 (angleBank, angleYaw, anglePitch);
+		Vector3 currentAngle = transform.eulerAngles;
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Rotate (Vector3.right * bankSpeed * Time.deltaTime);
-		}
+		// Set the rotation using Lerp. (No idea what it is, don't worry).
+		currentAngle = new Vector3 (
+			Mathf.LerpAngle (currentAngle.x, targetAngle.x, Time.deltaTime * tiltSpeed),
+			Mathf.LerpAngle (currentAngle.y, targetAngle.y, Time.deltaTime * tiltSpeed),
+			Mathf.LerpAngle (currentAngle.z, targetAngle.z, Time.deltaTime * tiltSpeed)
+		);
 
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.Rotate (Vector3.forward * bankSpeed * Time.deltaTime);
-		}
-
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.Rotate (Vector3.back * bankSpeed * Time.deltaTime);
-		}
-
-		if (Input.GetKey (KeyCode.A)) {
-			transform.Rotate (0, -rotateSpeed, 0);
-		}
-
-		if (Input.GetKey (KeyCode.D)) {
-			transform.Rotate (0, rotateSpeed, 0);
-		}
+		// Set the rotation angle.
+		transform.eulerAngles = currentAngle;
 	}
 }
